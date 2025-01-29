c5a315cf036b9047dfbd001949dc425d5b3ea580
Girish Mogaveera <girish@girish>
2025-01-30 01:59:16 +0530

    image generation1

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import styles from './css/Home.module.css'
 
+
+
+
+
+
 const Home = () => {
+
 
   // json data storing object
   const [movieData, setMovieData] = useState(null);
 
+  // fetches the movies data from the text file afr loading the wap page
+  useEffect(() => {
+    fetch('./data.json')
+      .then((response) => response.json())
+      .then((data) => setMovieData(data));
+  }, [])
+
+
+
+
+
+
   // img path variable
-  const [framePath, setFramePath] = useState('./frames/2.png');
+  const [framePath, setFramePath] = useState('./frames/1.png');
   // movie name text
   const [movieText, setMovieText] = useState();
 
 
-  const nextMovie = () => {
-    const number = Math.floor(Math.random() * (100 - 0 + 1)); alert(number);
-  }
+
+
+
+
+
 
 
   // on submitting the movie name
-  const submit = () => {
-    fetch('./data.json')
-      .then((response) => response.json())
-      .then((data) => setMovieData(data))
-    alert("movie name submited")
+  const submitMovie = () => {
+
+
+    const number = Math.floor(Math.random() * (2 - 0 + 1));
+
+    console.log("Next Movie id : "+number)
     console.log(movieData)
+    setFramePath(`./frames/${number+1}.png`)
+
+
   }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -33,19 +69,37 @@ const Home = () => {
 
   return (
     <>
+
+
+
+
+
+
       <div id={styles.navbar}>
         <p id={styles.h1}>GuessTheFrame.com</p>
       </div>
 
+
+
+
+
       <div id={styles.container1}>
+
+
+
+
         <div id={styles.mainBox}>
           <img id={styles.frame} src={framePath} />
           <div id={styles.typeBox}>
             <input placeholder='enter movie title here' id={styles.movieText} type='text' value={movieText} onChange={(e) => { setMovieText(e.target.value) }} />
-            <button id={styles.submit} onClick={submit}>submit</button>
+            <button id={styles.submit} onClick={submitMovie}>submit</button>
           </div>
-          <button id={styles.next} onClick={nextMovie}>Next</button>
+          <button id={styles.next} onClick={submitMovie}>Next</button>
         </div>
+
+
+
+
         <div id={styles.statisticsTable}>
           <p id={styles.tableHead}>Statistics</p>
           <table>
